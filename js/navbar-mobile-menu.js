c6338a8ad196ed39852ae944b9aaeed47eaf920e
Peter PayteR Gašparík <payter86@gmail.com>
2019-03-30 08:40:15 +0100

    init

---
+++ js/navbar-mobile-menu.js
@@ -0,0 +1,11 @@
+/**
+ Mobile functionality for navbar
+ */
+
+class NavbarMobileMenu {
+  show() {
+    alert("ok")
+  }
+}
+
+export default;
--- js/navbar-mobile.js
+++
@@ -1,3 +0,0 @@
-/**
- Mobile functionality for navbar
- */
